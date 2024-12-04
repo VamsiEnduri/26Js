06ddf0ebf1a211647300884bb59c55871e93861b
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-12-04 14:12:56 +0530

    for loop

---
+++ loops(for)/index.js
@@ -0,0 +1,83 @@
+// console.log(1)
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5)
+// console.log(6)
+// console.log(7)
+// console.log(8)
+// console.log(9)
+// console.log(10)
+
+// for 
+
+
+// for(initialisation   ; condition   ; updation (++/--)   ) {
+
+// }
+  
+
+  // 1to 10
+
+// 1
+// 2
+// 3
+// 9
+// 10
+
+
+// for(var a=10;a>=1;a--){ //9  //0
+   
+// }
+// console.log(a)
+// 10
+//9
+// console.log(i);
+
+// for(var i=0;i<=10;i++){
+    
+// }
+// console.log(i)
+//1
+
+
+// for(i=-1; i>=-10 ;i--){  // -1>=-10 -2>=-10  -10>=-10  -11>=-10
+//     console.log(i)
+// }
+
+
+// for(i=-10; i<=-1 ;i++){  // -1>=-10 -2>=-10  -10>=-10  -11>=-10
+//     console.log(i)
+// }
+// var aSum=0
+// for(var b=1;b<=10;b++){
+//      aSum+=b;
+// }
+// console.log(aSum)
+// 1+2+3+4+5+6+7+8+9+10=?
+
+// var apro=1
+// for(var b=1;b<=10;b++){
+//      apro*=b;
+// }
+// console.log(apro)
+
+var sumEven=0;
+var sumOdd=0;
+for(i=1;i<=10;i++){
+     if(i % 2==0){
+           sumEven+=i
+     }else{
+        sumOdd+=i
+     }
+}
+console.log(sumEven,"even")
+console.log(sumOdd,"odd")
+
+
+
+var sum=0
+for(i=1;i<=3;i++){
+    sum+=i
+}
+console.log(sum)
