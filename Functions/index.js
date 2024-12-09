012cbc9aec6a2dc1ac52aa28ae22ad7e8d18461a
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-12-09 11:53:41 +0530

    functions in js

---
+++ Functions/index.js
@@ -0,0 +1,51 @@
+// // function vamsi(){
+// //     //code to exucte
+// // }
+// // vamsi()   // invoking r calling
+
+// var a="vamsi"
+// var b="ravi"
+
+// function addToCart(){
+//     var a="laptop"
+//     var b="mobile";
+//     var c="cricket bat";
+//     var d={
+//         laptop:a,
+//         mobile:b,
+//         cricketBat:c,
+//     }
+//     console.log(d)
+// }
+// addToCart()
+
+
+//function declaration r function statment
+// function xyz(){
+
+// }
+// xyz()
+
+//anonymus
+// function (){
+
+// }
+
+//function expression
+// var x=function (){
+
+// }
+
+//assigning a function declaration to a variable is called as named function expression (r) assigning a anonymusfunction with name to varaible is called named function expression
+// var x=function abc(){
+
+// }
+
+
+//  immediately ivoked function (IIFE)
+// (function x(){
+//     console.log("vamsi")
+// })()
+
+
+// what is ps
